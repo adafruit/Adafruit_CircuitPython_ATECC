57c325033747bc5541822911df8d0d5aaa9770fe
Florent Gallaire <fgallaire@gmail.com>
2020-02-28 03:42:44 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -62,5 +62,5 @@ setup(
     # simple. Or you can use find_packages().
     # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
     #       CHANGE `py_modules=['...']` TO `packages=['...']`
-    py_modules=['adafruit_atecc'],
+    packages=['adafruit_atecc'],
 )
