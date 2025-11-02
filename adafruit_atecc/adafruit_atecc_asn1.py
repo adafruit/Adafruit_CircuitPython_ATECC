adb555af03df80317eca0789af026d9e2c534ecf
Dan Halbert <halbert@adafruit.com>
2025-11-02 11:33:07 -0500

    Change exception type from TypeError to ValueError; check for negative total_len

--- adafruit_atecc/adafruit_atecc_asn1.py
+++ adafruit_atecc/adafruit_atecc_asn1.py
@@ -252,7 +252,7 @@ def issuer_or_subject_length(
     :param str org: Organization of certificate
     :param str org_unit: Organization unit of certificate
     :param str common: Common data of certificate
-    :raises: TypeError if return value is 0
+    :raises: ValueError if return value is <= 0
     :return: Total length of provided certificate information.
     """
     tot_len = 0
@@ -269,6 +269,6 @@ def issuer_or_subject_length(
     if common:
         tot_len += 11 + len(common)
     
-    if not tot_len:
-        raise TypeError("Provided length must be > 0")
+    if tot_len <= 0:
+        raise ValueError("Provided length must be > 0")
     return tot_len
